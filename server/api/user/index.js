5111b5fa357fdef59fb34f240a8f18bf4ff5bf02
sadioraymond <sadiowalyraymond@gmail.com>
2016-12-17 11:11:33 +0000

    Verification du Code

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -15,5 +15,4 @@ router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
 router.get('/:id', auth.isAuthenticated(), controller.show);
 router.post('/', controller.create);
 
-
 module.exports = router;
