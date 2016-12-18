31992c8bc4c8e7fce6e7e3b9ba07f640f7201a79
pach93 <dergui2006@hotmail.com>
2016-12-18 13:18:35 +0000

    fonction avoir les matieres par classe

--- server/api/cours/cours.controller.js
+++ server/api/cours/cours.controller.js
@@ -5,43 +5,57 @@ var Cours = require('./cours.model');
 var Mat = require('../matiere/matiere.model')
 
 // Get list of courss
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   Cours.find(function (err, courss) {
-    if(err) { return handleError(res, err); }
+    if (err) { return handleError(res, err); }
     return res.status(200).json(courss);
   });
 };
 
-exports.getClasseByCours = function(req, res) {
-  Cours.find({classe : req.params.id},function (err, courss) {
-    if(err) { return handleError(res, err); }
-       return res.status(200).json(courss);
+var khol = [];
+exports.getClasseByCours = function (req, res) {
+  Cours.find({ classe: req.params.id }, function (err, courss) {
+    if (err) { return handleError(res, err); }
+    else {
+      
+      courss.forEach(function(element) {
+        Mat.find({ _id: element.matiere }, function (err, matieres) {
+        if (err) { return handleError(res, err); }
+        else {
+          khol.push(matieres);
+  
+        }
+      });
+      }, this);
+       return res.status(200).json(khol);
+    }
+
   });
 };
 
 // Get a single cours
-exports.show = function(req, res) {
+exports.show = function (req, res) {
   Cours.findById(req.params.id, function (err, cours) {
-    if(err) { return handleError(res, err); }
-    if(!cours) { return res.status(404).send('Not Found'); }
+    if (err) { return handleError(res, err); }
+    if (!cours) { return res.status(404).send('Not Found'); }
     return res.json(cours);
   });
 };
 
 // Creates a new cours in the DB.
-exports.create = function(req, res) {
-  Cours.create(req.body, function(err, cours) {
-    if(err) { return handleError(res, err); }
+exports.create = function (req, res) {
+  Cours.create(req.body, function (err, cours) {
+    if (err) { return handleError(res, err); }
     return res.status(201).json(cours);
   });
 };
 
 // Updates an existing cours in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+exports.update = function (req, res) {
+  if (req.body._id) { delete req.body._id; }
   Cours.findById(req.params.id, function (err, cours) {
     if (err) { return handleError(res, err); }
-    if(!cours) { return res.status(404).send('Not Found'); }
+    if (!cours) { return res.status(404).send('Not Found'); }
     var updated = _.merge(cours, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
@@ -51,12 +65,12 @@ exports.update = function(req, res) {
 };
 
 // Deletes a cours from the DB.
-exports.destroy = function(req, res) {
+exports.destroy = function (req, res) {
   Cours.findById(req.params.id, function (err, cours) {
-    if(err) { return handleError(res, err); }
-    if(!cours) { return res.status(404).send('Not Found'); }
-    cours.remove(function(err) {
-      if(err) { return handleError(res, err); }
+    if (err) { return handleError(res, err); }
+    if (!cours) { return res.status(404).send('Not Found'); }
+    cours.remove(function (err) {
+      if (err) { return handleError(res, err); }
       return res.status(204).send('No Content');
     });
   });
