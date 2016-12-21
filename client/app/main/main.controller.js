cd11b8a33dccaa0ffee1d7d502bc3dd9cc09d2a9
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2016-12-21 11:01:42 +0100

    etape 2

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -66,31 +66,6 @@ angular.module('appChicoApp')
     $scope.disable=disable;
 
 
-
-
-
-    //---------------
-    $scope.user = null;
-    $scope.users = null;
-
-    $scope.loadUsers = function() {
-
-    // Use timeout to simulate a 650ms request.
-    return $timeout(function() {
-
-      $scope.users =  $scope.users  || [
-        { id: 1, name: 'Scooby Doo' },
-        { id: 2, name: 'Shaggy Rodgers' },
-        { id: 3, name: 'Fred Jones' },
-        { id: 4, name: 'Daphne Blake' },
-        { id: 5, name: 'Velma Dinkley' }
-      ];
-      }, 650);
-    }
-
-      //-------------------------------------------
-
-
       $scope.getColor = function($index) {
       var _d = ($index + 1) % 11;
       var bg = '';
