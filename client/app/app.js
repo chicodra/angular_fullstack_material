67fa80277bc71d0f18a67ecc62ede5cbaf7ae5b3
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2016-12-28 10:51:50 +0000

    classe

--- client/app/app.js
+++ client/app/app.js
@@ -1,79 +1,109 @@
 'use strict';
 
 angular.module('appChicoApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngAnimate',
-  'ngMessages',
-  'ngRoute',
-  'btford.socket-io',
-  'ngMaterial'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngAnimate',
+    'ngMessages',
+    'ngRoute',
+    'btford.socket-io',
+    'ngMaterial'
 ])
-  .config(function($mdIconProvider) {
-    $mdIconProvider
-      .iconSet('action', '../assets/iconsets/action-icons.svg', 24)
-      .iconSet('alert', '../assets/iconsets/alert-icons.svg', 24)
-      .iconSet('av', '../assets/iconsets/av-icons.svg', 24)
-      .iconSet('communication', '../assets/iconsets/communication-icons.svg', 24)
-      .iconSet('content', '../assets/iconsets/content-icons.svg', 24)
-      .iconSet('device', '../assets/iconsets/device-icons.svg', 24)
-      .iconSet('editor', '../assets/iconsets/editor-icons.svg', 24)
-      .iconSet('file', '../assets/iconsets/file-icons.svg', 24)
-      .iconSet('hardware', '../assets/iconsets/hardware-icons.svg', 24)
-      .iconSet('icons', '../assets/iconsets/icons-icons.svg', 24)
-      .iconSet('image', '../assets/iconsets/image-icons.svg', 24)
-      .iconSet('maps', '../assets/iconsets/maps-icons.svg', 24)
-      .iconSet('navigation', '../assets/iconsets/navigation-icons.svg', 24)
-      .iconSet('notification', '../assets/iconsets/notification-icons.svg', 24)
-      .iconSet('social', '../assets/iconsets/social-icons.svg', 24)
-      .iconSet('toggle', '../assets/iconsets/toggle-icons.svg', 24)
-      .iconSet('avatar', '../assets/iconsets/avatar-icons.svg', 128);
-  })
-  .config(function ($routeProvider, $locationProvider, $httpProvider) {
-    $routeProvider
-      .otherwise({
-        redirectTo: '/'
-      });
+    .config(function($mdIconProvider) {
+        $mdIconProvider
+            .iconSet('action', '../assets/iconsets/action-icons.svg', 24)
+            .iconSet('alert', '../assets/iconsets/alert-icons.svg', 24)
+            .iconSet('av', '../assets/iconsets/av-icons.svg', 24)
+            .iconSet('communication', '../assets/iconsets/communication-icons.svg', 24)
+            .iconSet('content', '../assets/iconsets/content-icons.svg', 24)
+            .iconSet('device', '../assets/iconsets/device-icons.svg', 24)
+            .iconSet('editor', '../assets/iconsets/editor-icons.svg', 24)
+            .iconSet('file', '../assets/iconsets/file-icons.svg', 24)
+            .iconSet('hardware', '../assets/iconsets/hardware-icons.svg', 24)
+            .iconSet('icons', '../assets/iconsets/icons-icons.svg', 24)
+            .iconSet('image', '../assets/iconsets/image-icons.svg', 24)
+            .iconSet('maps', '../assets/iconsets/maps-icons.svg', 24)
+            .iconSet('navigation', '../assets/iconsets/navigation-icons.svg', 24)
+            .iconSet('notification', '../assets/iconsets/notification-icons.svg', 24)
+            .iconSet('social', '../assets/iconsets/social-icons.svg', 24)
+            .iconSet('toggle', '../assets/iconsets/toggle-icons.svg', 24)
+            .iconSet('avatar', '../assets/iconsets/avatar-icons.svg', 128);
+    })
+    .config(function($routeProvider, $locationProvider, $httpProvider) {
+        $routeProvider
+            .otherwise({
+                redirectTo: '/'
+            });
 
-    $locationProvider.html5Mode(true);
-    $httpProvider.interceptors.push('authInterceptor');
-  })
+        $locationProvider.html5Mode(true);
+        $httpProvider.interceptors.push('authInterceptor');
+    })
+    .config(function($mdThemingProvider) {
 
-  .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
-    return {
-      // Add authorization token to headers
-      request: function (config) {
-        config.headers = config.headers || {};
-        if ($cookieStore.get('token')) {
-          config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
-        }
-        return config;
-      },
+        $mdThemingProvider.definePalette('amazingPaletteName', {
+            '50': 'E1F5FE',
+            '100': 'B3E5FC',
+            '200': '81D4FA',
+            '300': '4FC3F7',
+            '400': '29B6F6',
+            '500': '03A9F4',
+            '600': '039BE5',
+            '700': '0288D1',
+            '800': '0277BD',
+            '900': '01579B',
+            'A100': '80D8FF',
+            'A200': '40C4FF',
+            'A400': '00B0FF',
+            'A700': '0091EA',
+            'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+            // on this palette should be dark or light
 
-      // Intercept 401s and redirect you to login
-      responseError: function(response) {
-        if(response.status === 401) {
-          $location.path('/login');
-          // remove any stale tokens
-          $cookieStore.remove('token');
-          return $q.reject(response);
-        }
-        else {
-          return $q.reject(response);
-        }
-      }
-    };
-  })
+            'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                '200', '300', '400', 'A100'],
+            'contrastLightColors': undefined    // could also specify this if default was 'dark'
+        });
 
-  .run(function ($rootScope, $location, Auth) {
-    // Redirect to login if route requires auth and you're not logged in
-    $rootScope.$on('$routeChangeStart', function (event, next) {
-      Auth.isLoggedInAsync(function(loggedIn) {
-        if (next.authenticate && !loggedIn) {
-          event.preventDefault();
-          $location.path('/login');
-        }
-      });
+        $mdThemingProvider.theme('default')
+            .primaryPalette('amazingPaletteName')
+            // .backgroundPalette('amazingPaletteName')
+
+    })
+
+    .factory('authInterceptor', function($rootScope, $q, $cookieStore, $location) {
+        return {
+            // Add authorization token to headers
+            request: function(config) {
+                config.headers = config.headers || {};
+                if ($cookieStore.get('token')) {
+                    config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+                }
+                return config;
+            },
+
+            // Intercept 401s and redirect you to login
+            responseError: function(response) {
+                if (response.status === 401) {
+                    $location.path('/login');
+                    // remove any stale tokens
+                    $cookieStore.remove('token');
+                    return $q.reject(response);
+                }
+                else {
+                    return $q.reject(response);
+                }
+            }
+        };
+    })
+
+    .run(function($rootScope, $location, Auth) {
+        // Redirect to login if route requires auth and you're not logged in
+        $rootScope.$on('$routeChangeStart', function(event, next) {
+            Auth.isLoggedInAsync(function(loggedIn) {
+                if (next.authenticate && !loggedIn) {
+                    event.preventDefault();
+                    $location.path('/login');
+                }
+            });
+        });
     });
-  });
--- client/app/classe/classe.controller.js
+++ client/app/classe/classe.controller.js
@@ -2,197 +2,189 @@
 
 angular.module('appChicoApp')
 
-  .controller('ClasseCtrl', function ($scope, $routeParams, $http, $location, socket, Cycle, Domaine, Niveau, sousDomaine, Matiere, Chapitre, Lecon) {
-    $scope.classeLib = $routeParams.classeLib;
-    $scope.cyclelib = $routeParams.cycle;
-    $scope.visible = false;
-    $scope.toggleLeft = buildToggler('left');
-    $scope.toggleRight = buildToggler('right');
+    .controller('ClasseCtrl', function($scope, $routeParams, $http, $location, socket, Cycle, Domaine, Niveau, sousDomaine, Matiere, Chapitre, Lecon) {
+        $scope.classeLib = $routeParams.classeLib;
+        $scope.cyclelib = $routeParams.cycle;
+        $scope.visible = false;
+        $scope.toggleLeft = buildToggler('left');
+        $scope.toggleRight = buildToggler('right');
 
 
-    function buildToggler(componentId) {
-      return function () {
-        $mdSidenav(componentId).toggle();
-      }
-    }
-
-
-    function checkCycle(libelle) {
-      $scope.bool = false;
-
-      Cycle.listCycles().then(function (listCycles) {
-        $scope.cycles = listCycles;
-        console.log('cycles', $scope.cycles)
-
-        console.log('libelle', libelle);
-        for (var id in $scope.cycles) {
-
-          if ($scope.cycles[id].libelle == libelle) {
-            $scope.cycle = $scope.cycles[id];
-            console.log('cycle', $scope.cycles[id].libelle);
-            $scope.bool = true;
-
-          }
-        }
-
-      });
-
-
-    }
-
-    function checkNiveau(libelle) {
-      $scope.boolniv = false;
-      Niveau.listeNiveauByCycle($scope.cycle.id).then(function (listNiveau) {
-        $scope.niveaux = listNiveau;
-        console.log('niveau', $scope.niveaux)
-
-        console.log('libelle', libelle);
-        for (var id in $scope.niveaux) {
-
-          if ($scope.niveaux[id].libelle == libelle) {
-            console.log('niveau', $scope.niveaux[id].libelle);
-            $scope.boolniv = true;
-
-          }
+        function buildToggler(componentId) {
+            return function() {
+                $mdSidenav(componentId).toggle();
+            }
         }
 
 
-      });
+        function checkCycle(libelle) {
+            $scope.bool = false;
+
+            Cycle.listCycles().then(function(listCycles) {
+                $scope.cycles = listCycles;
+                console.log('cycles', $scope.cycles)
+
+                console.log('libelle', libelle);
+                for (var id in $scope.cycles) {
+
+                    if ($scope.cycles[id].libelle == libelle) {
+                        $scope.cycle = $scope.cycles[id];
+                        console.log('cycle', $scope.cycles[id].libelle);
+                        $scope.bool = true;
+
+                    }
+                }
+
+            });
 
 
-    }
-
-    function init() {
-      //console.log(Niveau.niveau);
-
-
-      if (Niveau.niveau == null) {
-        checkCycle($scope.cyclelib);
-        window.setTimeout(function () {
-
-          if ($scope.bool) {
-            console.log("true", $scope.bool);
-            checkNiveau($scope.classeLib)
-            window.setTimeout(function () {
-              console.log("classelib",$scope.boolniv);
-              if($scope.boolniv){
-                Niveau.getNiveauByLabel($scope.classeLib).then(function (niveau) {
-                  console.log("classe lib", niveau);
-                  Niveau.niveau = niveau;
-
-                });
-              }
-              else{$scope.$apply(function() { $location.path("/"); });}
-
-
-            },100)
-          }
-          else {
-            console.log("false", $scope.bool);
-            $scope.$apply(function() { $location.path("/"); });
-            //$location.path('/');
-            //console.log('url',$location.absUrl());
-          }
-          //
-
-
-        }, 200);
-
-
-
-      }
-      window.setTimeout(function () {
-        Domaine.listeDomaineByNiveau(Niveau.niveau.id).then(function (listDomaines) {
-          $scope.listDomaines = listDomaines;
-          console.log("liste domaines", $scope.listDomaines);
-          //domaineClick($scope.listDomaines[0]);
-          //clickCycle($scope.listCycles[1]);
-
-        })
-      }, 100);
-
-
-    }
-
-    $scope.init = init();
-
-    function domaineClick(domaine) {
-      initParams();
-      $scope.listSousDomaines = [];
-      console.log("on domaine click", domaine);
-      sousDomaine.listeSousDomaineByDomaine(domaine._id).then(function (listSousDomaines) {
-        console.log("list sous domaine", listSousDomaines);
-        $scope.listSousDomaines = listSousDomaines;
-        // sousDomaineClick(listSousDomaines[0]);
-
-      });
-
-
-    }
-
-    //domaineClick($scope.listDomaines[0]);
-    $scope.domaineClick = domaineClick;
-
-    function sousDomaineClick(sousdomaine) {
-      $scope.listChapitres = [];
-      Matiere.listeMatieresBySousDomaines(sousdomaine._id).then(function (listMatieres) {
-        console.log("list matieres", listMatieres);
-        $scope.listMatieres = listMatieres;
-        //MatiereClick(listMatieres[0]);
-
-
-      });
-
-    }
-
-    $scope.sousDomaineClick = sousDomaineClick;
-    function MatiereClick(matiere) {
-
-      console.log('matieres', matiere);
-      Chapitre.listeChapitresByMatiere(matiere._id).then(function (listChapitre) {
-        console.log(' liste chapitres', listChapitre);
-        $scope.listChapitres = listChapitre;
-        //ChapitreClick(listChapitre[0]);
-
-
-      });
-
-    }
-
-    $scope.MatiereClick = MatiereClick;
-
-    function ChapitreClick(chapitre) {
-      $scope.listLecons = [];
-      $scope.visible = true;
-      Lecon.listLeconsByChapitre(chapitre._id).then(function (listLecons) {
-        console.log("list lecons", listLecons);
-        $scope.listLecons = listLecons;
-
-
-      });
-
-    }
-
-    $scope.ChapitreClick = ChapitreClick;
-
-    $scope.collapseAll = function (domaine) {
-      for (var i in $scope.listDomaines) {
-        if ($scope.listDomaines[i] != domaine) {
-          $scope.listDomaines[i].expanded = false;
         }
 
-      }
-      domaine.expanded = !domaine.expanded;
-    };
-    function initParams() {
-      //$scope.listDomaines = [];
-      $scope.listSousDomaines = [];
-      $scope.listLecons = [];
-      $scope.listChapitres = [];
-      $scope.listMatieres = [];
+        function checkNiveau(libelle) {
+            $scope.boolniv = false;
+            Niveau.listeNiveauByCycle($scope.cycle.id).then(function(listNiveau) {
+                $scope.niveaux = listNiveau;
+                console.log('niveau', $scope.niveaux)
+
+                console.log('libelle', libelle);
+                for (var id in $scope.niveaux) {
+
+                    if ($scope.niveaux[id].libelle == libelle) {
+                        console.log('niveau', $scope.niveaux[id].libelle);
+                        $scope.boolniv = true;
+
+                    }
+                }
+            });
+
+        }
+
+        function init() {
+            //console.log(Niveau.niveau);
+
+            if (Niveau.niveau == null) {
+                checkCycle($scope.cyclelib);
+                window.setTimeout(function() {
+
+                    if ($scope.bool) {
+                        console.log("true", $scope.bool);
+                        checkNiveau($scope.classeLib)
+                        window.setTimeout(function() {
+                            console.log("classelib", $scope.boolniv);
+                            if ($scope.boolniv) {
+                                Niveau.getNiveauByLabel($scope.classeLib).then(function(niveau) {
+                                    console.log("classe lib", niveau);
+                                    Niveau.niveau = niveau;
+
+                                });
+                            }
+                            else { $scope.$apply(function() { $location.path("/"); }); }
 
 
-    }
+                        }, 100)
+                    }
+                    else {
+                        console.log("false", $scope.bool);
+                        $scope.$apply(function() { $location.path("/"); });
+                        //$location.path('/');
+                        //console.log('url',$location.absUrl());
+                    }
+                    //
 
-  })
+
+                }, 200);
+
+
+
+            }
+            window.setTimeout(function() {
+                Domaine.listeDomaineByNiveau(Niveau.niveau.id).then(function(listDomaines) {
+                    $scope.listDomaines = listDomaines;
+                    console.log("liste domaines", $scope.listDomaines);
+                    //domaineClick($scope.listDomaines[0]);
+                    //clickCycle($scope.listCycles[1]);
+
+                })
+            }, 100);
+
+        }
+        $scope.init = init();
+
+        function domaineClick(domaine) {
+            initParams();
+            $scope.listSousDomaines = [];
+            console.log("on domaine click", domaine);
+            sousDomaine.listeSousDomaineByDomaine(domaine._id).then(function(listSousDomaines) {
+                console.log("list sous domaine", listSousDomaines);
+                $scope.listSousDomaines = listSousDomaines;
+                // sousDomaineClick(listSousDomaines[0]);
+
+            });
+
+
+        }
+        //domaineClick($scope.listDomaines[0]);
+        $scope.domaineClick = domaineClick;
+
+        function sousDomaineClick(sousdomaine) {
+            $scope.listChapitres = [];
+            Matiere.listeMatieresBySousDomaines(sousdomaine._id).then(function(listMatieres) {
+                console.log("list matieres", listMatieres);
+                $scope.listMatieres = listMatieres;
+                //MatiereClick(listMatieres[0]);
+
+
+            });
+
+        }
+        $scope.sousDomaineClick = sousDomaineClick;
+
+        function MatiereClick(matiere) {
+
+            console.log('matieres', matiere);
+            Chapitre.listeChapitresByMatiere(matiere._id).then(function(listChapitre) {
+                console.log(' liste chapitres', listChapitre);
+                $scope.listChapitres = listChapitre;
+                //ChapitreClick(listChapitre[0]);
+
+
+            });
+
+        }
+        $scope.MatiereClick = MatiereClick;
+
+        function ChapitreClick(chapitre) {
+            $scope.listLecons = [];
+            $scope.visible = true;
+            Lecon.listLeconsByChapitre(chapitre._id).then(function(listLecons) {
+                console.log("list lecons", listLecons);
+                $scope.listLecons = listLecons;
+
+
+            });
+
+        }
+        $scope.ChapitreClick = ChapitreClick;
+
+        $scope.collapseAll = function(domaine) {
+            for (var i in $scope.listDomaines) {
+                if ($scope.listDomaines[i] != domaine) {
+                    $scope.listDomaines[i].expanded = false;
+                }
+
+            }
+            domaine.expanded = !domaine.expanded;
+        };
+
+        function initParams() {
+            //$scope.listDomaines = [];
+            $scope.listSousDomaines = [];
+            $scope.listLecons = [];
+            $scope.listChapitres = [];
+            $scope.listMatieres = [];
+
+
+        }
+
+    })
 
